bf2ed984b3247e71b0dba8c75e4c2f527b4e715f
bryan <bryan.hespel@condorcet.be>
2020-10-30 17:05:32 +0100

    création boutique

---
+++ Projet_hespel_bryan/dal/Boutique.cs
@@ -0,0 +1,24 @@
+﻿using Projet_hespel_bryan.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Projet_hespel_bryan.dal
+{
+    public class Boutique : DbContext
+    {
+         public Boutique() : base("Boutique")
+            {
+            }
+
+         public DbSet<Article> Articles { get; set; }
+         public DbSet<Catégorie> Catégories { get; set; }
+         public DbSet<User> Users { get; set; }
+
+         protected override void OnModelCreating(DbModelBuilder modelBuilder)
+          {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+          }
+        }
+    }
+}
+}
